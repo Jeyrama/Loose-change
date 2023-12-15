d5582c4694af7adaa8647175ca01c460293a9be9
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-14 19:54:22 -0800

    Update Loose-change.js

--- Loose-change.js
+++ Loose-change.js
@@ -21,3 +21,13 @@ Examples:
 
 
 // Solution
+
+function looseChange(cents){
+  let a={Nickels:0,Pennies:0,Dimes:0,Quarters:0};
+  if (cents <= 0) return a;
+  a.Quarters = Math.floor(cents/25);
+  a.Dimes = Math.floor(cents%25/10);
+  a.Nickels = Math.floor(cents%25%10/5);
+  a.Pennies = Math.floor(cents%25%10%5);
+  return a;
+}
