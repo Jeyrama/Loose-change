263633b868caa231883a56a1652c2b8fb09e2503
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-14 19:55:04 -0800

    Update Loose-change.js

--- Loose-change.js
+++ Loose-change.js
@@ -33,3 +33,12 @@ function looseChange(cents){
 }
 
 // or
+
+const looseChange = (cents) => (
+  cents = Math.max(cents, 0),
+  {
+    Quarters: cents / 25 | 0,
+    Dimes: cents % 25 / 10 | 0,
+    Nickels: cents % 25 % 10 / 5 | 0,
+    Pennies: cents % 5 | 0
+})
